007db9c6abee5b3655c7efcc513ca8934810e958
逐鹿科技 <1771430144@qq.com>
2020-06-11 19:52:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,223 @@
+# Xunsearch for PHP
+
+---
+[![](https://travis-ci.org/ShaoZeMing/xunsearch.svg?branch=master)](https://travis-ci.org/ShaoZeMing/xunsearch) 
+[![](https://img.shields.io/packagist/v/ShaoZeMing/xunsearch.svg)](https://packagist.org/packages/shaozeming/xunsearch) 
+[![](https://img.shields.io/packagist/dt/ShaoZeMing/xunsearch.svg)](https://packagist.org/packages/stichoza/shaozeming/xunsearch)
+
+
+## 前置说明：
+
+### 请先安装xunsearch 服务端：
+
+- 普通编译安装(推荐)
+
+```
+
+wget http://www.xunsearch.com/download/xunsearch-full-latest.tar.bz2
+tar -xjf xunsearch-full-latest.tar.bz2
+
+cd xunsearch-full-1.4.10/
+sh setup.sh  
+//第一次安装的话，过程可能会稍显漫长，请不必着急，您大可泡杯茶一边喝一边等待即可。
+               
+cd $prefix ; bin/xs-ctl.sh restart
+
+
+更多详情请移步：http://www.xunsearch.com/doc/php/guide/start.installation
+```
+
+- docker 安装
+```$xslt
+Docker 方式安装服务端
+---------------------
+
+//下载映像：
+
+docker pull hightman/xunsearch
+
+//启动映像：
+
+docker run --name xunsearch -d \
+-p 8383:8383 -p 8384:8384 \
+-v /var/xunsearch/data:/usr/local/xunsearch/data \
+hightman/xunsearch:latest
+
+//以上启动参数可自行修改，该方式仅运行服务端
+
+```
+
+
+
+## Installing
+
+```shell
+$ composer require shaozeming/xunsearch -v
+```
+
+### configuration 
+
+-  老师搜索库配置 配置结构和官方ini文件类似，只不过替换为了php 数组形式方式展示，请照着官方文档和本配置文件对比，葫芦画瓢：http://www.xunsearch.com/doc/php/guide/ini.guide
+
+```php
+// config/xunsearch.php
+
+   
+    'default' => 'teacher',   //默认搜索库
+
+    'databases' => [
+
+//        老师搜索库配置 配置结构和官方ini文件类似，只不过替换为了php 数组形式方式展示，请照着官方文档和本配置文件对比，葫芦画瓢：http://www.xunsearch.com/doc/php/guide/ini.guide
+        'teacher' => [
+            'project.name' => 'teacher',
+            'project.default_charset' => 'utf-8',
+            'server.index' => '127.0.0.1:8383',
+            'server.search' => '127.0.0.1:8384',
+            'id' => [
+                'type' => 'id',
+            ],
+            'email' => [
+                'index' => 'mixed',
+            ],
+            'name' => [
+                'index' => 'mixed',
+            ],
+            'desc' => [
+                'index' => 'mixed',
+            ],
+
+        ],
+
+
+        //学生搜索库
+        'student' => [
+            'project.name' => 'student',
+            'project.default_charset' => 'utf-8',
+            'server.index' => '127.0.0.1:8383',
+            'server.search' => '127.0.0.1:8384',
+            'id' => [
+                'type' => 'id',
+            ],
+            'email' => [
+                'index' => 'mixed',
+            ],
+            'name' => [
+                'index' => 'mixed',
+            ],
+            'desc' => [
+                'index' => 'mixed',
+            ],
+
+        ],
+
+
+//        ...
+    ],
+
+```
+
+
+
+
+Example:
+
+```php
+
+        define('XS_APP_ROOT', 'your/config_file_path/dir/');  //可以定义配置文件目录
+        $xs = new XunsearchService();
+
+      
+       $data =[
+            ['id' => 1, 'email' => '928240096@qq.com', 'name' => 'Shao ZeMing 邵泽明 邵澤明', 'lesson' => '朗诵主持,Reciting Hosting,朗誦主持，','desc'=>'我是谁，我在哪儿，我要做什么，我不告诉你'],
+            ['id' => 2, 'email' => '12315@qq.com', 'name' => 'Chris Dong 董胜君  董勝君', 'lesson' => '朗诵主持,Reciting Hosting,朗誦主持，演講辯論，speech debate，演讲辩论','desc'=>'如果有一天，我走了，你应该知道我去了哪儿'],
+            ['id' => 3, 'email' => 'shao-ze-ming@outlook.com', 'name' => '二傻子 Two fools', 'lesson' => '朗诵主持,Reciting Hosting,朗誦主持，','desc'=>'最近头发掉的厉害，我该怎么办好呀'],
+            ['id' => 4, 'email' => 'szm19920426@qq.com', 'name' => '君莫笑 jun mo xiao 君莫笑', 'lesson' => '写作批改,writing correction,寫作批改,国学经典,National Classics,國學經典','desc'=>'哎呀，脑壳疼，脑壳疼，脑壳疼'],
+            ['id' => 5, 'email' => '1270912585@qq.com', 'name' => '李四，li si 李四', 'lesson' => '朗诵主持,Reciting Hosting,朗誦主持，演講辯論，speech debate，演讲辩论，国学经典,National Classics,國學經典','desc'=>'你知道我对你不静静是喜欢'],
+        ];
+         
+//        $result =  $xs->addIndex($data);  //添加索引
+//        $result = $xs->search('泽明');   //搜索默认索引数据库
+//        $result = $xs->setSort('id',true)->setLimit(15)->search('ming');  //搜索排序,或分页
+//        $result = $xs->setDatabase('student')->search('shao');  //搜索自定义索引数据库
+          $result = $xs->setDatabase('student')->search('shao');  //搜索自定义索引数据库
+
+        //$newData=[....]
+        //$result =  $xs->updateIndex($newData);  //更新单挑或多条索引
+        //$result =  $xs->delIndex('3');  //删除单挑或多条索引
+        //$result =  $xs->cleanIndex();   //清空索引
+
+        print_r($result);
+          * @return array  返回数组结构
+                    return [
+                              'doc'           => Object,      //搜索数据结果文档
+                              'hot'           => array,       //热门词汇
+                              'count'         => int,         //搜索结果统计
+                              'total'         => int,         //数据库总数据
+                              'corrected'     => array,       //搜索提示
+                              'related'       => array,       //相关搜索
+                              'search_cost'   => int,         //搜索所用时间
+                              'total_cost'    => int,         //页面所用时间
+                          ];
+                  */
+         
+        
+         
+         /*
+ 示例结果：
+Array
+(
+    [doc] => Array
+        (
+            [0] => XSDocument Object
+                (
+                    [_data:XSDocument:private] => Array
+                        (
+                            [id] => 1
+                            [email] => 928240096@qq.com
+                            [name] => Shao ZeMing 邵泽明 邵澤明
+                            [lesson] => 朗诵主持,Reciting Hosting,朗誦主持，
+                            [desc] => 我是谁，我在哪儿，我要做什么，我不告诉你
+                            [255] => 
+                        )
+
+                    [_terms:XSDocument:private] => 
+                    [_texts:XSDocument:private] => 
+                    [_charset:XSDocument:private] => UTF-8
+                    [_meta:XSDocument:private] => Array
+                        (
+                            [docid] => 1
+                            [rank] => 1
+                            [ccount] => 0
+                            [percent] => 100
+                            [weight] => 1.1440536975861
+                        )
+
+                )
+
+        )
+
+    [hot] => Array
+        (
+        )
+
+    [count] => 1
+    [total] => 5
+    [corrected] => Array
+        (
+        )
+
+    [related] => Array
+        (
+        )
+
+    [search_cost] => 0.00080204010009766
+    [total_cost] => 0.004767894744873
+)
+ */
+
+```
+
+## License
+
+MIT
+
---
+++ src/XunsearchService.php
@@ -0,0 +1,345 @@
+<?php
+
+namespace ShaoZeMing\Xunsearch;
+
+
+
+// +----------------------------------------------------------------------
+// | ChaseAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2020~2030 温州逐鹿科技有限公司 [ https://www.chasedeer.cn ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://www.chasedeer.cn
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/chasechan/ChaseAdmin
+// +----------------------------------------------------------------------
+
+include_once __DIR__ . '/vendor/lib/XS.php';
+
+class XunsearchService extends \XS implements XunsearchInterface
+{
+
+    protected $flush_index = true;//立即刷新索引
+    protected $fuzzy = true;//开启模糊搜索
+    protected $auto_synonyms = true; //开启自动同义词搜索功能
+    protected $limit = 10; //每页搜索条数
+    protected $offset = 0; //每页搜索条数
+    protected $sort_field = null; //排序字段
+    protected $sort_state = false; //排序规则:false 倒叙，true 正序
+    protected $database = null; //连接服务器
+
+    /**
+     * XunsearchService constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        defined('XS_APP_ROOT') || define('XS_APP_ROOT', dirname(__DIR__) . '/config');
+
+
+        $file = XS_APP_ROOT.'/xunsearch.php';
+        if(!file_exists($file)){
+            throw new \Exception('配置文件'.$file.'不存在');
+        }
+        $config = include $file;
+
+        if(!isset($config['default'])){
+            throw new \Exception('配置文件'.$file.'缺少default配置');
+        }
+        $this->database = $config['default'];
+
+        $config = $config['databases'][$config['default']];
+
+        parent::__construct($config);
+
+    }
+
+
+    /**
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param $string
+     * @return array|mixed
+     * @throws \XSException
+     */
+    public function search($string)
+    {
+        if (!$string) {
+            throw new \Exception('请输入搜索字符');
+        }
+        $count = $total = $search_cost = 0;
+        $doc = $related = $corrected = $hot = [];
+        $total_begin = microtime(true);
+        $search = $this->getSearch();
+
+        //热门词汇
+        $hot = $search->getHotQuery();
+
+        // fuzzy search 模糊搜索
+        $search->setFuzzy($this->fuzzy);
+
+        // synonym search
+        $search->setAutoSynonyms($this->auto_synonyms);
+
+        // set query
+        $search->setQuery($string);
+
+
+        if($this->sort_field){
+            $search->setSort($this->sort_field,$this->sort_state);
+        }
+        // get the result
+        $search_begin = microtime(true);
+        $doc = $search->setLimit($this->limit, $this->offset)->search();
+        $search_cost = microtime(true) - $search_begin;
+
+        // get other result
+        $count = $search->getLastCount();    //最近一次搜索结果数
+        $total = $search->getDbTotal();      //数据库总数
+
+
+
+        // try to corrected, if resul too few
+        if ($count < 1 || $count < ceil(0.001 * $total)) {
+            $corrected = $search->getCorrectedQuery();
+        }
+        // get related query
+        $related = $search->getRelatedQuery();
+        $total_cost = microtime(true) - $total_begin;
+
+        return [
+            'doc' => $doc,                    //搜索数据结果文档
+            'hot' => $hot,                    //热门词汇
+            'count' => $count,                  //搜索结果统计
+            'total' => $total,                  //数据库总数据
+            'corrected' => $corrected,             //搜索提示
+            'related' => $related,               //相关搜索
+            'search_cost' => $search_cost,          //搜索所用时间
+            'total_cost' => $total_cost,           //页面所用时间
+        ];
+    }
+
+
+    /**
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param array $data
+     * @return \XSIndex
+     * @throws \XSException
+     */
+    public function addIndex(array $data)
+    {
+        if (!is_array($data)) {
+            throw new \Exception('你的索引参数不是一个数组');
+        }
+        if (isset($data[0]) && is_array($data[0])) {
+            // 多维数组
+            foreach ($data as $v) {
+                $this->getIndex()->add(new \XSDocument($v));
+            }
+        } else {
+            // 一维数组
+            $this->getIndex()->add(new \XSDocument($data));
+        }
+        //索引是否立即生效
+        if ($this->flush_index) {
+            $this->getIndex()->flushIndex();
+        }
+        return $this->getIndex();
+    }
+
+
+    /**
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param array $data
+     * @return \XSIndex
+     * @throws \XSException
+     */
+    public function updateIndexOne(array $data)
+    {
+        if (!is_array($data)) {
+            throw new \Exception('你的索引参数不是一个数组');
+        }
+
+        if (count($data) == count($data, 1)) {
+            // 一维数组
+            $this->getIndex()->update(new \XSDocument($data));
+        }
+
+        //索引是否立即生效
+        if ($this->flush_index) {
+            $this->getIndex()->flushIndex();
+        }
+
+        return $this->getIndex();
+    }
+
+
+    /**
+     * 删除索引文档
+     * User: shaozeming
+     * @param array|string $ids 删除主键值为 $pids 的记录
+     * @return \XSIndex
+     * @throws \XSException
+     */
+    public function delIndex($ids)
+    {
+
+        if (!$ids) {
+            throw new \Exception('索引主键不能为空');
+        }
+
+        $this->getIndex()->del($ids);
+        if ($this->config['flushIndex']) {
+            $this->getIndex()->flushIndex();
+        }
+        return $this->getIndex();
+    }
+
+
+    /**
+     * 清空索引数据
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @return \XSIndex
+     */
+    public function cleanIndex()
+    {
+
+        return $this->getIndex()->clean();
+
+    }
+
+
+    /**
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param int $limit
+     * @param int $offset
+     * @return $this
+     */
+    public function setLimit($limit = 10, $offset = 0)
+    {
+
+        $this->limit = $limit;
+        $this->offset = $offset;
+
+        return $this;
+    }
+
+    /**
+     * 模糊搜索
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param bool $state
+     * @return $this
+     */
+    public function setFuzzy($state = true)
+    {
+        $this->fuzzy = $state;
+        return $this;
+    }
+
+    /**
+     * 自动匹配同义词
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param bool $state
+     * @return $this
+     */
+    public function setAutoSynonyms($state = true)
+    {
+        $this->auto_synonyms = $state;
+        return $this;
+    }
+
+    /**
+     * 排序
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param $field
+     * @param bool $sort
+     * @return $this
+     */
+    public function setSort($field,$sort = false)
+    {
+        $this->sort_field = $field;
+        $this->sort_state = $sort;
+        return $this;
+    }
+
+
+    /**
+     * 设置配置
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param array $config
+     * @return $this
+     * @throws \XSException
+     */
+    public function setConfig(array $config)
+    {
+
+        $this->loadIniFile($config);
+        self::$_lastXS = $this;
+        return $this;
+    }
+
+
+    /**
+     * 设置bug
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param $database
+     * @return XunsearchService
+     * @throws \XSException
+     */
+    public function setDatabase($database){
+
+        $file = XS_APP_ROOT.'/xunsearch.php';
+        if(!file_exists($file)){
+            throw new \Exception('配置文件'.$file.'不存在');
+        }
+        $config = include $file;
+        if(!isset($config['databases'][$database])){
+            throw new \Exception('配置文件'.$file.'缺少这个'.$database.'配置');
+        }
+
+        $this->database = $database;
+
+        $config = $config['databases'][$database];
+
+        return $this->setConfig($config);
+
+    }
+
+    /**
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param $attr
+     * @param $value
+     * @return $this
+     */
+    public function __set($attr, $value)
+    {
+        $this->$attr = $value;
+        return $this;
+    }
+
+
+    /**
+     * User: ZeMing Shao
+     * Email: szm19920426@gmail.com
+     * @param $attr
+     * @return mixed
+     */
+    public function __get($attr)
+    {
+        return $this->$attr;
+    }
+
+
+}
